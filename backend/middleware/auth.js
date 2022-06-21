fc51c8436598a3fd1a3fba1ec885186005e12536
Vinod Kumar Halvi <hvinodkumar3354@gmail.com>
2022-06-21 15:10:02 +0530

    error

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -37,7 +37,7 @@
 
 
    
-const ErrorHandler = require("../utils/errorhandler");
+const ErrorHandler = require("../utils/ErrorHandler");
 const catchAsyncErrors = require("./catchAsyncErrors");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModules");
